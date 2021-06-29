18adfbdaefeb65ca1873c5b42b0519189ee30969
Jose Pereira <onaips@gmail.com>
2021-06-29 10:17:06 +0100

    Update licence header

--- src/SettingsViewController.swift
+++ src/SettingsViewController.swift
@@ -1,10 +1,18 @@
-//
-//  SettingsWindow.swift
-//  dmenu-mac
-//
-//  Created by Jose Pereira on 2/23/16.
-//  Copyright © 2016 Jose Pereira. All rights reserved.
-//
+/*
+ * Copyright (c) 2016 Jose Pereira <onaips@gmail.com>.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, version 3.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
 
 import Cocoa
 
